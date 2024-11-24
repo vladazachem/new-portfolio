3fa9fe011b0ded318bab1d6cd9e27652836217d5
vladazachem <71698265+vladazachem@users.noreply.github.com>
2024-11-24 16:47:35 +0100

    edit the size of logo eye

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -7,11 +7,12 @@ export default function Intro() {
                 </h1>
                 <h1>Developer & Web Designer</h1>
             </div>
-            <svg id="point" viewBox="0 0 244 244" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="122" cy="122" r="122" fill="#2570BB" />
-                <circle cx="122" cy="122" r="88" fill="#F06543" />
-                <circle cx="121.5" cy="121.5" r="52.5" fill="#EDE8E4" />
-                <circle cx="122" cy="123" r="14" fill="#181410" />
+            <svg id="point" width="380" height="380" viewBox="0 0 244 244" fill="none"
+                 xmlns="http://www.w3.org/2000/svg">
+                <circle cx="122" cy="122" r="122" fill="#2570BB"/>
+                <circle cx="122" cy="122" r="88" fill="#F06543"/>
+                <circle cx="121.5" cy="121.5" r="52.5" fill="#EDE8E4"/>
+                <circle cx="122" cy="123" r="14" fill="#181410"/>
             </svg>
             <div className="divider-line"></div>
         </section>
