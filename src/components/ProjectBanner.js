9aa212bcdbea41ebdabe67a5cd145a17ab7e959d
vladazachem <71698265+vladazachem@users.noreply.github.com>
2024-11-25 14:42:16 +0100

    complete the layout of banner

---
+++ src/components/ProjectBanner.js
@@ -0,0 +1,32 @@
+import Image from "next/image";
+
+export default function ProjectBanner({ project }) {
+    return (
+        <div className="flex flex-col gap-4 px-21 items-center">
+            <div
+                className="relative h-[510px] w-[759px] rounded-[30px] overflow-hidden flex justify-center items-center">
+                <Image
+                    src={project.banner}
+                    alt={project.name}
+                    key={project.id}
+                    loading="eager"
+                    fill
+                    sizes="759px"
+                    style={{ objectFit: "cover", objectPosition: "center" }}
+                    priority
+                />
+            </div>
+            <div className="flex flex-row justify-between w-[759px]">
+                <div className="flex flex-col gap-4">
+                    <h5 className="text-lg">{project.name}</h5>
+                    <h5 className="text-lg">{project.role}</h5>
+                </div>
+                <div className="flex flex-col gap-4">
+                    <h5 className="text-lg text-right">{project.year}</h5>
+                    <h5 className="text-lg text-right">{project.type}</h5>
+                </div>
+            </div>
+        </div>
+
+    );
+}
