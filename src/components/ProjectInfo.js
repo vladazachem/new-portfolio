30467aa2cc7a3539f73f3f39c3bfdefaf79a6167
vladazachem <71698265+vladazachem@users.noreply.github.com>
2024-11-25 16:56:56 +0100

    add bottom padding

--- src/components/ProjectInfo.js
+++ src/components/ProjectInfo.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function ProjectInfo({ project }) {
     return (
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center pb-20">
             <div className="flex flex-row justify-between w-[60%] py-[140px]">
                 <h5 className="font-bold">Project overview</h5>
                 <div className="w-[75%]">
