cfb912aff629d0bfe4079de20fda2ec02dd52494
vladazachem <71698265+vladazachem@users.noreply.github.com>
2024-11-28 16:12:40 +0100

    add version of the icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,17 +3,17 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 export const metadata = {
-    title: 'Vlada Sarokina | Junior Front-End Developer & Web Designer',
-    description: 'Portfolio of Vlada Sarokina, showcasing projects and skills.',
+    title: "Vlada Sarokina | Junior Front-End Developer & Web Designer",
+    description: "Portfolio of Vlada Sarokina, showcasing projects and skills.",
     icons: {
-      icon: '/logo.png'
+      icon: "/logo.png?v=1.0"
     },
 };
 
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body className="global-padding font-poppins bg-main text-text">
+        <body className="global-padding font-poppins bg-main text-text">
                 <Header />
                 {children}
                 <Footer />
