e2091ba50d2ab2c87a6ba5fc43025b7c05f7eb5e
vladazachem <71698265+vladazachem@users.noreply.github.com>
2024-11-29 17:22:24 +0100

    add text animation

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,13 +1,56 @@
+"use client"
+
+import {useEffect, useRef} from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 export default function Intro() {
+    const textContainerRef = useRef(null);
+
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger);
+
+        const lines = textContainerRef.current.querySelectorAll("h1");
+
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: textContainerRef.current,
+                start: "top 80%",
+                end: "top 20%",
+                toggleActions: "play none none reverse",
+                markers: false,
+            }
+        })
+
+        tl.fromTo(
+            lines,
+            { y: 100, opacity: 0 },
+            { y: 0, opacity: 1, ease:"power2.out", stagger: 0.5 }
+        )
+
+        return () => {
+            ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
+        }
+    }, []);
+
     return (
         <section>
-            <div className="flex flex-col gap-10 py-32">
-                <h1>
-                    <b className="font-medium text-[60px]">I&apos;m a </b>
-                    Junior Front-End
-                </h1>
-                <h1>Developer &</h1>
-                <h1 className="text-center">Web Designer</h1>
+            <div
+                className="flex flex-col gap-10 py-32"
+                ref={textContainerRef}
+            >
+                <div className="overflow-hidden">
+                    <h1>
+                        <b className="font-medium text-[60px]">I&apos;m a </b>
+                        Junior Front-End
+                    </h1>
+                </div>
+                <div className="overflow-hidden">
+                    <h1>Developer &</h1>
+                </div>
+                <div className="overflow-hidden">
+                    <h1 className="text-center">Web Designer</h1>
+                </div>
             </div>
             <div className="z-0 absolute top-[340px] right-[50px]">
                 <svg id="point" width="380" height="380" viewBox="0 0 244 244" fill="none"
