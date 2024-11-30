9ccf0e3c1f9485c31ba98021fd68a791c092918e
vladazachem <71698265+vladazachem@users.noreply.github.com>
2024-11-30 12:26:57 +0100

    add project description mapping

--- src/components/ProjectInfo.js
+++ src/components/ProjectInfo.js
@@ -3,10 +3,12 @@ import Image from "next/image";
 export default function ProjectInfo({ project }) {
     return (
         <div className="flex flex-col items-center pb-20">
-            <div className="flex flex-row justify-between w-[60%] py-[140px]">
+            <div className="flex flex-row justify-between w-[900px] py-[140px]">
                 <h5 className="font-bold">Project overview</h5>
                 <div className="w-[75%]">
-                    <h5>{project.description}</h5>
+                    {project.description.map((desc, index) => (
+                        <h5 key={index} className="mb-2">{desc}</h5>
+                    ))}
                 </div>
             </div>
             <div className="flex justify-center items-center rounded-[30px] overflow-hidden">
