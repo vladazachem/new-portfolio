0b008e2815b3014148ca17b6a7b5f41f9282f0f7
vladazachem <71698265+vladazachem@users.noreply.github.com>
2024-11-30 13:35:17 +0100

    add screen breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,8 @@
-/** @type {import('tailwindcss').Config} */
+// @ts-check
+/**
+ * @type {import('tailwindcss').Config}
+ */
+
 module.exports = {
   content: [
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -11,6 +15,15 @@ module.exports = {
         text: '#181410',
         blue: '#2570BB',
         highlight: '#F06543',
+      },
+      screens: {
+        '2xl': { max: '1700px' },
+        'xl': { max: '1360px' },
+        'lg': { max: '1134px' },
+        'md': { max: '970px' },
+        'sm': { max: '784px' },
+        'xs': { max: '600px' },
+        'xxs': { max: '530px' },
       },
     },
   },
