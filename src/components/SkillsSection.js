fdca4639c92104e3d6064ad55e81c384a85227aa
vladazachem <71698265+vladazachem@users.noreply.github.com>
2024-12-01 15:08:55 +0100

    when screen is xxs 3 cols and 5 rows

--- src/components/SkillsSection.js
+++ src/components/SkillsSection.js
@@ -4,9 +4,11 @@ export default function SkillsSection() {
     return (
         <section id="skills" className="py-24">
             <h4 className="font-semibold mb-16">01 Software Skills</h4>
-            <div className="grid grid-cols-4 grid-rows-4 grid-flow-col gap-8">
+            <div className="grid grid-cols-4 grid-rows-4 grid-flow-col gap-8
+            xxs:grid-cols-3 xxs:grid-rows-5
+            ">
                 {skills.map((skill, index) => (
-                    <p key={index} className="text-lg">
+                    <p key={index} className="text-lg sm:text-sm">
                         {skill}
                     </p>
                 ))}
