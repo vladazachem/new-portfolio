ac4c1de08eef606b569fffda78935a5e407fde79
vladazachem <71698265+vladazachem@users.noreply.github.com>
2025-02-07 16:35:04 +0100

    fix: adjust layout and sizing in ProjectInfo component for better responsiveness

--- src/components/ProjectInfo.js
+++ src/components/ProjectInfo.js
@@ -3,9 +3,9 @@ import Image from "next/image";
 export default function ProjectInfo({ project }) {
     return (
         <div className="flex flex-col items-center pb-20">
-            <div className="flex flex-row sm:flex-col justify-between w-full py-36 px-[5%] sm:py-28 sm:px-0 sm:gap-6">
+            <div className="flex flex-row md:flex-col justify-between w-full py-36 sm:py-28 md:px-0 md:gap-8">
                 <h5 className="font-bold">Project overview</h5>
-                <div className="w-[75%] sm:w-full">
+                <div className="w-[900px] md:w-full">
                     {project.description.map((desc, index) => (
                         <h5 key={index} className="mb-2 xs:text-base">{desc}</h5>
                     ))}
